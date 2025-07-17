3427a44275bb28ecea24302c03ed250d293d5756
Rethinavel Pandian S <rethinavelpandiyan702@gmail.com>
2025-07-17 08:15:50 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,38 +1,45 @@
-from random import*
+from random import randint,choice,shuffle
 import string
-password=""
-fun_lst=[]
-def capital():
-	global password;password+=choice(string.ascii_uppercase)
-def small():
-	global password;password+=choice(string.ascii_lowercase)
-def number_fun():
-	global password;password+=str(randint(0,9))
-def symbol():
-	global password;password+=choice(string.punctuation)
-print('#'+'-'*20+"Password Generator"+'-'*20+'#')
-try:
-	pass_length=int(input("\nPassword length: "))
-	if pass_length<4:
-		raise Exception("Password length is too small.\nMinimum length is 4.")
-	if pass_length>50:
-		raise Exception("Password length is too high.\nMaximum length is 50.")
-except Exception as error:
-	print(f"Error: {error}")
-	exit()
-print("\nYes type [Y/y] or No type [N/n].")
-capital_alpha=input("\nCapital alphapet: ").upper()
-if capital_alpha=='Y':fun_lst.append(capital)
-small_alpha=input("\nSmall alphapet: ").upper()
-if small_alpha=='Y':fun_lst.append(small)
-number=input("\nNumbers: ").upper()
-if number=='Y':fun_lst.append(number_fun)
-special_char=input("\nSpecial charater: ").upper()
-if special_char=='Y':fun_lst.append(symbol)
-for i in range(pass_length):
-		shuffle(fun_lst)
-		for fun in fun_lst:
-			fun()
-			if len(password)==pass_length:
-				print(f"\nYour Password: {password}")
-				exit()
+class PasswordGenerator:
+	def __init__(self):
+		self.password=""
+		self.fun_lst=[]
+	def capital(self):
+		return(choice(string.ascii_uppercase))
+	def small(self):
+		return(choice(string.ascii_lowercase))
+	def number_fun(self):
+		return(str(randint(0,9)))
+	def symbol(self):
+		return(choice(string.punctuation))
+	def start(self):
+		print('#'+'-'*20+"Password Generator"+'-'*20+'#')
+		try:
+			pass_length=int(input("\nPassword length: "))
+			if pass_length<4:
+				raise Exception("Password length is too small.\nMinimum length is 4.")
+			if pass_length>50:
+				raise Exception("Password length is too high.\nMaximum length is 50.")
+		except Exception as error:
+			print(f"Error: {error}")
+			exit()
+		print("\nYes type [Y/y] or No type [N/n].")
+		capital_alpha=input("\nCapital alphapet? ").upper()
+		if capital_alpha=='Y':self.fun_lst.append(self.capital)
+		small_alpha=input("\nSmall alphapet? ").upper()
+		if small_alpha=='Y':self.fun_lst.append(self.small)
+		number=input("\nNumbers? ").upper()
+		if number=='Y':self.fun_lst.append(self.number_fun)
+		special_char=input("\nSpecial charater? ").upper()
+		if special_char=='Y':self.fun_lst.append(self.symbol)
+		for i in range(pass_length):
+				shuffle(self.fun_lst)
+				for fun in self.fun_lst:
+					self.password+=fun()
+					if len(self.password)==pass_length:
+						print(f"\nYour Password: {self.password}")
+						exit()
+if __name__=="__main__":
+	pg=PasswordGenerator()
+	pg.start()
+	
